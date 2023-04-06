5c6660aa7bc0c68f624dac9b49a05a8da395609a
JustLeif <leif@elliottnet.com>
2023-04-06 16:28:51 -0400

    localhost:5796/tip now returns a JSON representation of the cardano-node tip.

--- cardano-node-api/src/main.rs
+++ cardano-node-api/src/main.rs
@@ -1,21 +1,32 @@
 use tide::Request;
-use tide::prelude::*;
+use std::{process::Command};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
-struct Animal {
-    name: String,
-    legs: u16,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Tip {
+    pub block: usize,
+    pub epoch: usize,
+    pub era: String,
+    pub hash: String,
+    pub slot: usize,
+    pub syncProgress: String
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
-    app.at("/").post(order_shoes);
-    app.listen("0.0.0.0:").await?;
+    app.at("/tip").get(get_tip);
+    app.listen("127.0.0.1:5796").await?;
     Ok(())
 }
 
-async fn order_shoes(mut req: Request<()>) -> tide::Result {
-    let Animal { name, legs } = req.body_json().await?;
-    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+async fn get_tip(mut _req: Request<()>) -> tide::Result {
+    let query_tip = Command::new("cardano-cli")
+                                        .arg("query")
+                                        .arg("tip")
+                                        .arg("--mainnet")
+                                        .output()
+                                        .unwrap();
+    let tip: Tip = serde_json::from_str(&String::from_utf8_lossy(&query_tip.stdout)).unwrap();
+    Ok(format!("{}", serde_json::to_string(&tip).unwrap()).into())
 }
